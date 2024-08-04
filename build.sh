afb018e6a53f44fc92fb51d8929ae009cf78a9d5
Saheel Sapovadia <saheelsapovadia23@gmail.com>
2024-08-04 23:18:18 +0530

    ci cd scritps

--- build.sh
+++ build.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+npm install
+
 # Replace with the actual path to your React project
 REACT_PROJECT_DIR="./frontend"
 
 # Change to the React project directory
 cd "$REACT_PROJECT_DIR"
 
+npm install
+
 # Build the React project
 npm run build
 
 # Copy the built files to the target directory
 cp -r dist/* ../src/frontend
+
+cd ..
+
+npm run start
